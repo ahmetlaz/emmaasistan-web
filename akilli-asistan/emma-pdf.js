7076a3093847e281758b5b1e274fb7f21b8c9ee4
ahmetlaz <ahmetizban@gmail.com>
2025-08-29 20:59:36 +0300

    Delete akilli-asistan directory

--- akilli-asistan/emma-pdf.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("download-pdf").onclick = function() {
-  let doc = new window.jspdf.jsPDF();
-  let chatText = Array.from(document.querySelectorAll("#chat div")).map(div => div.innerText).join("\n\n");
-  doc.text(chatText, 10, 10);
-  doc.save("emma_chat.pdf");
-};
